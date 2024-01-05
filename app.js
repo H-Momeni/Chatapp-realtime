e4cddda4e18e1fe370adba23b78bc57f53b7cbe8
H-Momeni <hannan.mgh@gmail.com>
2024-01-05 18:15:12 +0330

    first

---
+++ app.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/Chat')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = 3000;
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+const Message = require('C://Users//H.MGH//Desktop//chatapp-main-master//models//message.js');
+const User = require('C://Users//H.MGH//Desktop//chatapp-main-master//models//user.js');
+
+
+io.on('connection', async (socket) => {
+  console.log('User connected:', socket.id);
+
+  
+
+  try {
+    // Fetch previous messages from the database
+    const previousMessages = await Message.find().sort({ timestamp: 1 }).exec();
+
+    // Send previous messages to the newly connected client
+    socket.emit('previous messages', previousMessages);
+  } catch (error) {
+    console.error('Error fetching previous messages:', error);
+  }
+
+  // Listen for incoming chat messages
+  socket.on('chat message', async (data) => {
+    console.log('Received message:', data);
+    try {
+      // Save the message to MongoDB using async/await
+      const message = new Message({ user: data.user, text: data.message });
+      
+      //const message = new Message({ user: userID, text: data.message });  problem when fetching
+
+      await message.save();
+      console.log('Message saved to the database');
+
+
+      /* const user1=new User({name:data.user});
+       await user1.save();
+       console.log('user save');*/
+
+
+      // Broadcast the message to all connected clients
+      io.emit('chat message', data);
+    } catch (error) {
+      console.error('Error saving message to database:', error);
+    }
+  });
+
+  let userID;
+  socket.on('user save', async (data) => {
+
+    try {
+
+
+      const user1 = new User({ name: data.user });
+      await user1.save();
+       userID=user1._id;
+
+
+      console.log('users save', user1._id);
+
+
+
+    } catch (error) {
+      console.error('Error saving user to database:', error);
+    }
+  });
+  
+
+
+  socket.on('clearDocuments', async () => {
+    try {
+      // Implement the code to clear documents in your MongoDB collection
+      await Message.deleteMany({}); // This assumes your model is named 'Message'
+      io.emit('messagesCleared');
+      console.log('All documents cleared from the database.');
+    } catch (error) {
+      console.error('Error clearing documents:', error);
+    }
+  });
+
+
+
+  // Listen for user disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+app.use(express.static('public'));
---
+++ public/scriptSocket.js
@@ -0,0 +1,68 @@
+
+function showAlert(message) {
+    alert(message);
+};
+const socket = io();
+
+// Handle incoming chat messages
+socket.on('chat message', (data) => {
+    const messages = document.getElementById('messages');
+    const li = document.createElement('li');
+    li.textContent = `${data.user}: ${data.message}`;
+    messages.appendChild(li);
+});
+
+// Handle receiving previous messages
+socket.on('previous messages', (previousMessages) => {
+    const messages = document.getElementById('messages');
+    previousMessages.forEach((msg) => {
+        const li = document.createElement('li');
+        li.textContent = `${msg.user}: ${msg.text}`;
+        messages.appendChild(li);
+    });
+});
+
+socket.on('messagesCleared', () => {
+    const messages = document.getElementById('messages');
+    messages.innerHTML = ''; // Clear the chat interface for this client
+});
+
+// Handle form submission
+document.getElementById('messageForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.getElementById('messageInput');
+    const message = input.value.trim();
+
+    const input1 = document.getElementById('name');
+    const message1 = input1.value.trim();
+
+    //const
+
+
+
+    if (message !== '' && message1 !== '') {
+        socket.emit('chat message', { user: message1, message });
+        input.value = '';
+        document.getElementById('firstnameInput').style.display = 'none';
+    }
+    else if (message1 === '') {
+        showAlert('please enter your username!');
+    }
+    else if (message === '') {
+        showAlert('please enter your message!');
+    }
+});
+document.getElementById('submitButton').addEventListener('click', (e) => {
+    if (e.target.type === 'button' && e.target.textContent === 'clear History Chat') {
+        socket.emit('clearDocuments');
+    }
+});
+
+
+document.getElementById('logbtn').addEventListener('click', (e) => {
+    const input1 = document.getElementById('name');
+    const message1 = input1.value.trim();
+    if (message1 !== '') {
+        socket.emit('user save', { user: message1 });
+    }
+});
